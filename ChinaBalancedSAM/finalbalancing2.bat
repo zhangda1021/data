a2e532c54b333404df1da7af6e38d42c0a8c4199
Da Zhang <zhangda10215@gmail.com>
2012-03-20 23:23:17 +0800

    Mar 20

--- ChinaBalancedSAM/finalbalancing2.bat
+++ ChinaBalancedSAM/finalbalancing2.bat
@@ -6,7 +6,7 @@ title	Balancing energy data for %1 -- job started at %time%
 call gams finalbalancing2 --prov=%1 o=listings\%1_balance.lst gdx=data\gdx\finalbalancing2\%1
 :next
 
-pause
+:pause
 shift
 
 if not "%1"=="" goto start
